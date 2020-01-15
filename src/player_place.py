b1c7045d097c0d0cac353ad7ffe19b180951592a
tturon973 <tturon@ucvts.org>
2020-01-15 11:32:23 -0500

    player movement not finished

--- src/player_place.py
+++ src/player_place.py
@@ -11,9 +11,12 @@ class Player:
 
     def update(self):
         self.pix_pos += self.direction
+        self.grid_pos = (self.pix_pos[0]+TOP_BOTTOM_MARGIN)//self.app.cell_width+1
 
     def draw(self):
         pygame.draw.circle(self.app.screen, PLAYER_COLOR, (int(self.pix_pos.x), int(self.pix_pos.y)), self.app.cell_width//2-2)
+        pygame.draw.rect(self.app.screen, RED, (self.grid_pos[0]*self.app.cell_width+TOP_BOTTOM_MARGIN//2, self.grid_pos[1]*self.app.cell_height+TOP_BOTTOM_MARGIN//2, self.app.cell_width, self.app.cell_height))
+
 
     def move(self, direction):
         self.direction = direction
