071f4bffbd759dbdea281f59d7d1d0ef7f94823f
tturon973 <58041229+tturon973@users.noreply.github.com>
2020-01-15 17:11:05 -0500

    part 7 done

--- src/player_place.py
+++ src/player_place.py
@@ -11,10 +11,15 @@ class Player:
 
     def update(self):
         self.pix_pos += self.direction
-        self.grid_pos[0] = (self.pix_pos[0]+TOP_BOTTOM_MARGIN)//self.app.cell_width+1
+
+        #setting grid position in reference to pixel position.  as the player moves, there is something tracking it
+        self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_MARGIN+self.app.cell_width//2)//self.app.cell_width+1
+        self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_MARGIN+self.app.cell_height//2)//self.app.cell_height+1
 
     def draw(self):
         pygame.draw.circle(self.app.screen, PLAYER_COLOR, (int(self.pix_pos.x), int(self.pix_pos.y)), self.app.cell_width//2-2)
+
+        #drawing the grid pos rect 
         pygame.draw.rect(self.app.screen, RED, (self.grid_pos[0]*self.app.cell_width+TOP_BOTTOM_MARGIN//2, self.grid_pos[1]*self.app.cell_height+TOP_BOTTOM_MARGIN//2, self.app.cell_width, self.app.cell_height))
 
 
